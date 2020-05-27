83453f44acfc1ecad30d505c53d199f86939b7a8
Becca <rrdoukas@gmail.com>
2020-05-27 16:33:35 -0500

    adds actors

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock"})
+
+actor.save
+
+#  actor = Actor.new(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
+
+#  actor = Actor.new(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
+
+#  actor = Actor.new(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
+
+#  actor = Actor.new(first_name: "Nathan", last_name: "Fillion", known_for: "Firefly")
