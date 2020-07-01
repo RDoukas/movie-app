304e0df53a6bc345d912a4e14e3815ef0cf52eab
Becca <rrdoukas@gmail.com>
2020-07-01 15:52:05 -0500

    adds to heroku

--- http.rb
+++ http.rb
@@ -1,11 +1,11 @@
 require "http"
 
-response = HTTP.get("http://localhost:3000/api/one_movie_url")
+response = HTTP.get("https://hudson-keener-2984/api/one_movie_url")
 
 puts response
 
 require "http"
 
-response = HTTP.get("http://localhost:3000/api/all_movies_url")
+response = HTTP.get("https://hudson-keener-2984/api/all_movies_url")
 
 puts response
